0046827764fd32cb4976b2c586c40e36fc47f466
hechagdani <hechagdani@alvariumgroup.com>
2023-07-14 09:33:38 +0100

    to progress jobRessource

---
+++ app/Filament/Resources/JobResource.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\JobResource\Pages;
+use App\Filament\Resources\JobResource\RelationManagers;
+use App\Models\Job;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class JobResource extends Resource
+{
+    protected static ?string $model = Job::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('user_id')
+                    ->numeric()
+                    ->required(),
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TagsInput::make('tags')
+                    ->separator(',')
+                    ->required(),
+                Forms\Components\TextInput::make('company')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('location')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('website')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('description')
+                    ->required(),
+                Forms\Components\TextInput::make('logo')
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('user_id')->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('title'),
+                Tables\Columns\TextColumn::make('tags'),
+                Tables\Columns\TextColumn::make('company'),
+                Tables\Columns\TextColumn::make('location'),
+                Tables\Columns\TextColumn::make('email'),
+                Tables\Columns\TextColumn::make('website'),
+                Tables\Columns\TextColumn::make('description'),
+                Tables\Columns\TextColumn::make('logo'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListJobs::route('/'),
+            'create' => Pages\CreateJob::route('/create'),
+            'edit' => Pages\EditJob::route('/{record}/edit'),
+        ];
+    }
+}
